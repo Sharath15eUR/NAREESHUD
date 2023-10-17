0a02f2541ea795b10f9422cff157d177c629a382
NAREESH-UD <93960137+NAREESH-UD@users.noreply.github.com>
2023-10-17 14:41:55 -0400

    WEEK 11 and 12 PROGRAMMING ASSIGNMENTS

---
+++ EmbedUR-Programming-in-C-NPTEL-main/WEEK 10 PROGRAMMING ASSIGNMENTS/WEEK 12 PROGRAMMING ASSIGNMENTS/Assignment_3.c
@@ -0,0 +1,34 @@
+/*Write a C program to store n elements using Dynamic Memory Allocation - calloc() and find the Largest element*/
+
+Ans:
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n; 
+    float *element;
+
+    scanf("%d", &n); 
+    element=(float*) calloc(n,sizeof(float));
+    if(element==NULL)
+    {
+       printf("Error!!! memory not allocated.");
+       exit(0);
+    }
+    int i;
+    for(i=0;i<n;i++)
+    {
+       scanf("%f",element+i);
+    }
+    for(i=1;i<n;i++)
+    {
+       if(*element<*(element+i))
+          *element=*(element+i);
+    }
+    printf("Largest element = %.2f",*element);
+    return 0;
+}
+
+
